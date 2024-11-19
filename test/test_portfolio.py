191e6688d50481026c0775d3bc229c9449e1a444
ndlorenc <ndlorenc@icloud.com>
2024-11-19 13:47:51 -0500

    [ndlorenc] Started test_portfolio.py tests

--- test/test_portfolio.py
+++ test/test_portfolio.py
@@ -1,4 +1,17 @@
+import mock
+from mock.mock import patch
+from telebot import types
 from code import portfolio
 
-def test_buyStock():
-    portfolio.buyStock("MSFT", 1)
+@patch("telebot.telebot")
+def test_run(mock_telebot, mocker):
+    mc = mock_telebot.return_value
+    mc.reply_to.return_value = True
+    message = create_message("hello from test run!")
+
+def create_message(text):
+    params = {"messagebody": text}
+    chat = types.User(11, False, "test")
+    message = types.Message(1, None, None, chat, "text", params, "")
+    message.text = text
+    return message
