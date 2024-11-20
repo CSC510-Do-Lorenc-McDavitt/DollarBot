aee3b119c66a6c69041c43cd8c7924de0046269e
jfmcdavitt <mcdavittjacob@gmail.com>
2024-11-20 00:21:31 -0500

    Merge stuff for helper again

--- code/helper.py
+++ code/helper.py
@@ -42,6 +42,9 @@ budget_options = {"update": "Add/Update", "view": "View", "delete": "Delete"}
 budget_types = {"overall": "Overall Budget", "category": "Category-Wise Budget"}
 data_format = {"data": [], "budget": {"overall": "0", "category": None}}
 analytics_options = {"overall": "Overall budget split by Category", "spend": "Split of current month expenditure", "remaining": "Remaining value", "history": "Time series graph of spend history"}
+portfolio_options = {"buy": "Buy a Stock",
+                     "sell": "Sell a Stock", "view": "View Portfolio"}
+portfolio_format = {"stocks": []}
 
 # set of implemented commands and their description
 commands = {
