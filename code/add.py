33b42ad62461ee39fe4abae505b9a9bbb88e1b22
tndo <tndo@ncsu.edu>
2024-11-24 01:06:11 -0500

    formatting a little bit

--- code/add.py
+++ code/add.py
@@ -305,9 +305,9 @@ def handle_group_email(message, bot, group_emails, per_member_share, group_name,
             mail_content = f'''Hello,
                         This email is to inform you of a split expense cost. Be sure
                         to pay it back soon!
-                        Current expense cost of group: {group_name}
+                        Current expense cost of group {group_name}:
                         The recent expense generated: ${"{:.2f}".format(split_amount)}
-                        What you owe: ${"{:.2f}".format(per_member_share)}
+                        What you owe for the group: ${"{:.2f}".format(per_member_share)}
                         Thank you!
                         '''
             # The mail addresses and password
